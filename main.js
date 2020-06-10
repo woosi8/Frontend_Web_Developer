5bba5c6b952f2208eb3bc55e89f63efebe5313d6
iriver89 <tmfvmehek@gmail.com>
2020-06-10 22:50:49 +0900

    JS: Transparent home

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Make navbar transparent when it is on the top + Top-arrow button
+// Make navbar transparent when it is on the top + Show "arrow up" bittpm wjem scrp;;omg dpwm
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 const topElem = document.querySelector(".top-arrow");
@@ -18,17 +18,19 @@ document.addEventListener("scroll", () => {
     }
 });
 
-btt.addEventListener("click", function (e) {
+// Handle click on the "arrow up" button
+btt.addEventListener("click", (e) => {
     e.preventDefault();
-    scrollTop();
+    scrollIntoView("#home");
+    // scrollTop();
 });
 
-function scrollTop() {
-    if (window.pageYOffset > 0) {
-        window.scrollBy(0, -50);
-        setTimeout(scrollTop, 0);
-    }
-}
+// function scrollTop() {
+//     if (window.pageYOffset > 0) {
+//         window.scrollBy(0, -50);
+//         setTimeout(scrollTop, 0);
+//     }
+// }
 // End
 
 // Handle scrolling when tapping on the navbar menu
@@ -58,15 +60,25 @@ function zoomOut(event) {
 }
 // End
 
-
 // Handle click on :contact me: button on home
-const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click',() => {
-    scrollIntoView('#contact');
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+    scrollIntoView("#contact");
 });
+// End
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+// End
+
+// Common Functions
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollTo.scrollIntoView({ behavior: "smooth" });
 }
 // End
