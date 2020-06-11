779784ea01d505549b1c34f57efbe89c4b1a043a
iriver89 <tmfvmehek@gmail.com>
2020-06-11 22:51:46 +0900

    JS:work filtering

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const btt = document.querySelector("#back-to-top");
 let scrollPos;
 
 document.addEventListener("scroll", () => {
-    console.log(`navbarHeight :${navbarHeight}`);
+    // console.log(`navbarHeight :${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add("navbar--dark");
         topElem.classList.add("visible");
@@ -17,6 +17,7 @@ document.addEventListener("scroll", () => {
         topElem.classList.remove("visible");
     }
 });
+// End// End// End// End// End// End// End// End
 
 // Handle click on the "arrow up" button
 btt.addEventListener("click", (e) => {
@@ -31,13 +32,13 @@ btt.addEventListener("click", (e) => {
 //         setTimeout(scrollTop, 0);
 //     }
 // }
-// End
+// End// End// End// End// End// End// End// End
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
     const target = event.target;
-    const link = target.dataset.link;
+    const link = target.dataset.link; // 먼저 html에서 data-link로 연결해준다
     if (link == null) {
         return;
     }
@@ -45,7 +46,7 @@ navbarMenu.addEventListener("click", (event) => {
     // scrollTo.scrollIntoView({ behavior: "smooth" });
     scrollIntoView(link);
 });
-// End
+// End// End// End// End// End// End// End// End
 
 // Home-Img Mouseover
 function zoomIn(event) {
@@ -58,14 +59,14 @@ function zoomOut(event) {
     event.target.style.zIndex = 0;
     event.target.style.transition = "all 0.5s";
 }
-// End
+// End// End// End// End// End// End// End// End
 
 // Handle click on :contact me: button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
-// End
+// End// End// End// End// End// End// End// End
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
@@ -74,11 +75,35 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
-// End
+// End// End// End// End// End// End// End// End
+
+// Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const proejects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter =
+        e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add("anim-out");
+    setTimeout(() => {
+        proejects.forEach((project) => {
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+        projectContainer.classList.remove("anim-out");
+    }, 300);
+});
+// End// End// End// End// End// End// End// End
 
 // Common Functions
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" });
 }
-// End
+// End// End// End// End// End// End// End// End
