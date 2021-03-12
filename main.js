f46b5bd9fb0962d299f5a45b6ce1c916f49731bc
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-03-12 23:17:50 +0900

    Add card-maker in my work

--- main.js
+++ main.js
@@ -10,67 +10,67 @@ console.log(window.scrollY);
 console.log(navbarHeight);
 
 document.addEventListener("scroll", () => {
-  // console.log(`navbarHeight :${navbarHeight}`);
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
-    topElem.classList.add("visible");
-  } else {
-    navbar.classList.remove("navbar--dark");
-    topElem.classList.remove("visible");
-  }
+	// console.log(`navbarHeight :${navbarHeight}`);
+	if (window.scrollY > navbarHeight) {
+		navbar.classList.add("navbar--dark");
+		topElem.classList.add("visible");
+	} else {
+		navbar.classList.remove("navbar--dark");
+		topElem.classList.remove("visible");
+	}
 });
 // End// End// End// End// End// End// End// End
 
 ///////////////////////////////// Common Functions
 function scroll(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+	const scrollTo = document.querySelector(selector);
+	scrollTo.scrollIntoView({ behavior: "smooth" });
 }
 // End// End// End// End// End// End// End// End
 
 ///////////////////////////////// Handle click on the "arrow up" button
 btt.addEventListener("click", (e) => {
-  e.preventDefault();
-  scroll("#home");
-  // scrollTop();
+	e.preventDefault();
+	scroll("#home");
+	// scrollTop();
 });
 // End// End// End// End// End// End// End// End
 
 ///////////////////////////////// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
-  const target = event.target;
-  const link = target.dataset.link; // 먼저 html에서 data-link로 연결해준다
+	const target = event.target;
+	const link = target.dataset.link; // 먼저 html에서 data-link로 연결해준다
 
-  if (link == null) {
-    // link가 null이면 아무것도 하지않고 리턴해서 더이상 밑에 있는 코드가 실행되지 않도록 한다
-    return;
-  }
-  // const scrollTo = document.querySelector(link);
-  // scrollTo.scrollIntoView({ behavior: "smooth" });
-  navbarMenu.classList.remove("open"); // 네브바 클릭시 항상 창이 닫힐수있도록(반응형에서)
-  scroll(link);
+	if (link == null) {
+		// link가 null이면 아무것도 하지않고 리턴해서 더이상 밑에 있는 코드가 실행되지 않도록 한다
+		return;
+	}
+	// const scrollTo = document.querySelector(link);
+	// scrollTo.scrollIntoView({ behavior: "smooth" });
+	navbarMenu.classList.remove("open"); // 네브바 클릭시 항상 창이 닫힐수있도록(반응형에서)
+	scroll(link);
 });
 // End// End// End// End// End// End// End// End
 
 ///////////////////////////////// Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+	navbarMenu.classList.toggle("open");
 });
 
 // End// End// End// End// End// End// End// End
 
 ///////////////////////////////// Home-Img Mouseover
 function zoomIn(event) {
-  event.target.style.transform = "scale(1.2)";
-  event.target.style.zIndex = 1;
-  event.target.style.transition = "all 0.5s";
+	event.target.style.transform = "scale(1.2)";
+	event.target.style.zIndex = 1;
+	event.target.style.transition = "all 0.5s";
 }
 function zoomOut(event) {
-  event.target.style.transform = "scale(1)";
-  event.target.style.zIndex = 0;
-  event.target.style.transition = "all 0.5s";
+	event.target.style.transform = "scale(1)";
+	event.target.style.zIndex = 0;
+	event.target.style.transition = "all 0.5s";
 }
 // End// End// End// End// End// End// End// End
 
@@ -86,8 +86,8 @@ const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  // 공식만든거임: 내려가는 스크롤 값에 따라 opacity값에 반영되도록. 예) 1- 400/800 = opacity 0.5
-  home.style.opacity = 1 - window.scrollY / homeHeight;
+	// 공식만든거임: 내려가는 스크롤 값에 따라 opacity값에 반영되도록. 예) 1- 400/800 = opacity 0.5
+	home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 // End// End// End// End// End// End// End// End
 
@@ -112,67 +112,67 @@ const proejects = document.querySelectorAll(".project"); //컨텐츠 각각 data
 const projectsArray = [...proejects];
 const categoryCount = document.querySelectorAll(".category__count");
 workBtnContainer.addEventListener("click", (e) => {
-  const filter =
-    // button에는 data-filter값이 있지만 그 안에 span태그에는 없어서
-    // p태그는 btn클릭 범주안에 들어가있어서 된다.
-    // 클릭한곳이 즉, e값이 span이라면 span의 부모 즉 button에 data-filter값을 가져온다
-    e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    // 혹시 몰라서 filter가 null 이면 아무것도 하지않기
-    return;
-  }
+	const filter =
+		// button에는 data-filter값이 있지만 그 안에 span태그에는 없어서
+		// p태그는 btn클릭 범주안에 들어가있어서 된다.
+		// 클릭한곳이 즉, e값이 span이라면 span의 부모 즉 button에 data-filter값을 가져온다
+		e.target.dataset.filter || e.target.parentNode.dataset.filter;
+	if (filter == null) {
+		// 혹시 몰라서 filter가 null 이면 아무것도 하지않기
+		return;
+	}
 
-  // Remove selection from the previous item and select the new one
-  const selectElem = document.querySelector(".category__btn.selected");
-  if (selectElem != null) {
-    selectElem.classList.remove("selected");
-  }
-  e.target.classList.add("selected");
+	// Remove selection from the previous item and select the new one
+	const selectElem = document.querySelector(".category__btn.selected");
+	if (selectElem != null) {
+		selectElem.classList.remove("selected");
+	}
+	e.target.classList.add("selected");
 
-  // 필터링해서 걸려진것들에 anim-out을 준다 (먼저 anim이 된다음에 아래 0.3초후에 효과가 사라진다)
-  projectContainer.classList.add("anim-out");
-  setTimeout(() => {
-    // foreach 는 quersysleectorAll에 있는 값들을 배열형태로 가져온다
-    proejects.forEach((project) => {
-      // 만약 위에서 선택한 filter가 * 이거나 선택한 data-type과 똑같으면 (즉 같은 필터값을 나오게)
-      if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("invisible"); //타입이 동일한 클래스에 invisible 제거해서 보이게하기
-      } else {
-        project.classList.add("invisible");
-      }
-    });
-    // 클릭시 0.3초후에 anim-out 이 사라지고 본 내용이 돌아오게한다. 그래서 효과가 보이게 한다
-    projectContainer.classList.remove("anim-out");
-  }, 300);
+	// 필터링해서 걸려진것들에 anim-out을 준다 (먼저 anim이 된다음에 아래 0.3초후에 효과가 사라진다)
+	projectContainer.classList.add("anim-out");
+	setTimeout(() => {
+		// foreach 는 quersysleectorAll에 있는 값들을 배열형태로 가져온다
+		proejects.forEach((project) => {
+			// 만약 위에서 선택한 filter가 * 이거나 선택한 data-type과 똑같으면 (즉 같은 필터값을 나오게)
+			if (filter === "*" || filter === project.dataset.type) {
+				project.classList.remove("invisible"); //타입이 동일한 클래스에 invisible 제거해서 보이게하기
+			} else {
+				project.classList.add("invisible");
+			}
+		});
+		// 클릭시 0.3초후에 anim-out 이 사라지고 본 내용이 돌아오게한다. 그래서 효과가 보이게 한다
+		projectContainer.classList.remove("anim-out");
+	}, 300);
 });
 
 // Make category numbers automatically when the proejcts add
 const categoryBtn = document.querySelectorAll(".category__btn");
 let typeCss = projectsArray.filter((value) => value.dataset.type === "css");
 let typeVjs = projectsArray.filter((value) => value.dataset.type === "vjs");
-let typeNote = projectsArray.filter((value) => value.dataset.type === "note");
+let typeNote = projectsArray.filter((value) => value.dataset.type === "react");
 categoryBtn.forEach((btn) => {
-  const child = btn.childNodes[3];
-  switch (btn.dataset.filter) {
-    case "*":
-      child.innerText = projectsArray.length;
-      break;
-    case "css":
-      child.innerText = typeCss.length;
-      break;
-    case "vjs":
-      child.innerText = typeVjs.length;
-      break;
-    case "note":
-      child.innerText = typeNote.length;
-      break;
-    default:
-      break;
-  }
-  // if (btn.dataset.filter === 'css') {
-  //     child.innerText = typeCss.length;
-  //     console.log(child);
-  // }
+	const child = btn.childNodes[3];
+	switch (btn.dataset.filter) {
+		case "*":
+			child.innerText = projectsArray.length;
+			break;
+		case "css":
+			child.innerText = typeCss.length;
+			break;
+		case "vjs":
+			child.innerText = typeVjs.length;
+			break;
+		case "react":
+			child.innerText = typeNote.length;
+			break;
+		default:
+			break;
+	}
+	// if (btn.dataset.filter === 'css') {
+	//     child.innerText = typeCss.length;
+	//     console.log(child);
+	// }
 });
 
 // End// End// End// End// End// End// End// End
@@ -191,55 +191,55 @@ let currentIndex = 0;
 let topHeight = 0;
 
 function calculateTallestSlide() {
-  for (let i = 0; i < slideCount; i++) {
-    if (slides[i].offsetHeight > topHeight) {
-      topHeight = slides[i].offsetHeight;
-    }
-  }
-  sliderContainer.style.height = topHeight + "px";
-  sliderWrapper.style.height = topHeight + "px";
+	for (let i = 0; i < slideCount; i++) {
+		if (slides[i].offsetHeight > topHeight) {
+			topHeight = slides[i].offsetHeight;
+		}
+	}
+	sliderContainer.style.height = topHeight + "px";
+	sliderWrapper.style.height = topHeight + "px";
 }
 calculateTallestSlide();
 
 function slideLeft() {
-  for (let j = 0; j < slideCount; j++) {
-    slides[j].style.left = j * 100 + "%";
-  }
+	for (let j = 0; j < slideCount; j++) {
+		slides[j].style.left = j * 100 + "%";
+	}
 }
 
 slideLeft();
 
 function goToSlide(idx) {
-  sliderContainer.style.left = idx * -100 + "%";
-  currentIndex = idx;
-  sliderContainer.classList.add("animated");
+	sliderContainer.style.left = idx * -100 + "%";
+	currentIndex = idx;
+	sliderContainer.classList.add("animated");
 
-  // 기존의 active가 설정된 점을 찾고
-  const pagerAct = document.querySelector(".dot.dot_active");
-  // 지금 이동한 인덱스의 점을 찾고
-  const dot = document.querySelector(`.dot[data-index="${idx}"]`);
-  pagerAct.classList.remove("dot_active");
-  dot.classList.add("dot_active");
+	// 기존의 active가 설정된 점을 찾고
+	const pagerAct = document.querySelector(".dot.dot_active");
+	// 지금 이동한 인덱스의 점을 찾고
+	const dot = document.querySelector(`.dot[data-index="${idx}"]`);
+	pagerAct.classList.remove("dot_active");
+	dot.classList.add("dot_active");
 }
 
 const pageDots = document.querySelectorAll(".dot");
 
 navPrev.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (currentIndex != 0) {
-    goToSlide(currentIndex - 1);
-  } else {
-    goToSlide(slideCount - 1);
-  }
+	event.preventDefault();
+	if (currentIndex != 0) {
+		goToSlide(currentIndex - 1);
+	} else {
+		goToSlide(slideCount - 1);
+	}
 });
 
 navNext.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (currentIndex < slideCount - 1) {
-    goToSlide(currentIndex + 1);
-  } else {
-    goToSlide(0);
-  }
+	event.preventDefault();
+	if (currentIndex < slideCount - 1) {
+		goToSlide(currentIndex + 1);
+	} else {
+		goToSlide(0);
+	}
 });
 
 goToSlide(0);
@@ -249,35 +249,35 @@ goToSlide(0);
 ///////////////////////////////// Pager in Home
 const slidePager = document.querySelector(".slide_pagination");
 slidePager.addEventListener("click", (e) => {
-  const pagerAct = document.querySelector(".dot.dot_active");
-  if (pagerAct != null) {
-    pagerAct.classList.remove("dot_active");
-  }
-  e.target.parentNode.classList.add("dot_active");
+	const pagerAct = document.querySelector(".dot.dot_active");
+	if (pagerAct != null) {
+		pagerAct.classList.remove("dot_active");
+	}
+	e.target.parentNode.classList.add("dot_active");
 
-  const curIndex = Number(e.target.parentNode.getAttribute("data-index"));
-  goToSlide(curIndex);
+	const curIndex = Number(e.target.parentNode.getAttribute("data-index"));
+	goToSlide(curIndex);
 });
 
 // End
 
 ///////////////////////////////// Slide Show in Testimonial
 (function () {
-  const testElem = document.querySelector("#testimonials");
-  function showValue() {
-    const testmonials = document.querySelector(".testmonials");
-    let posY = testElem.getBoundingClientRect().top;
-    //  posY의 현재탑높이값이 브라우저 전체높이에 3/10 보다 작을경우, 즉 엘리먼트의 탑이 전체높이에 3/10만큼 남았을때 조건발동
-    if (posY < window.innerHeight * 0.3) {
-      testmonials.classList.add("active");
-    } else {
-      testmonials.classList.remove("active");
-    }
-  }
+	const testElem = document.querySelector("#testimonials");
+	function showValue() {
+		const testmonials = document.querySelector(".testmonials");
+		let posY = testElem.getBoundingClientRect().top;
+		//  posY의 현재탑높이값이 브라우저 전체높이에 3/10 보다 작을경우, 즉 엘리먼트의 탑이 전체높이에 3/10만큼 남았을때 조건발동
+		if (posY < window.innerHeight * 0.3) {
+			testmonials.classList.add("active");
+		} else {
+			testmonials.classList.remove("active");
+		}
+	}
 
-  window.addEventListener("scroll", function () {
-    showValue();
-  });
+	window.addEventListener("scroll", function () {
+		showValue();
+	});
 })();
 
 // End
@@ -290,25 +290,25 @@ const overlay = document.querySelectorAll(".modal__overlay");
 const closeBtn = document.querySelectorAll(".closeBtn");
 
 const openModal = () => {
-  event.currentTarget.nextElementSibling.classList.add("hidden");
+	event.currentTarget.nextElementSibling.classList.add("hidden");
 
-  // when you use node , you should be careful of line break
-  // event.currentTarget.parentNode.childNodes[9].classList.add('hidden')
+	// when you use node , you should be careful of line break
+	// event.currentTarget.parentNode.childNodes[9].classList.add('hidden')
 };
 const closeModal = (event) => {
-  event.currentTarget.parentNode.classList.remove("hidden");
+	event.currentTarget.parentNode.classList.remove("hidden");
 };
 overlay.forEach((event) => {
-  event.addEventListener("click", closeModal);
+	event.addEventListener("click", closeModal);
 });
 closeBtn.forEach((event) => {
-  event.addEventListener("click", closeModal);
+	event.addEventListener("click", closeModal);
 });
 homeBtn.forEach((event) => {
-  event.addEventListener("click", openModal);
+	event.addEventListener("click", openModal);
 });
 testBtn.forEach((event) => {
-  event.addEventListener("click", openModal);
+	event.addEventListener("click", openModal);
 });
 
 // Modal in Home - 2
@@ -339,63 +339,63 @@ const homeTitleElem = document.querySelector(".home__title");
 const dynamicElem = document.querySelector(".dynamic");
 const dynamicElem2 = document.querySelector(".dynamic2");
 function blink() {
-  homeTitleElem.classList.toggle("active");
+	homeTitleElem.classList.toggle("active");
 }
 setInterval(() => {
-  blink();
+	blink();
 }, 300);
 
 const stringArr = [dynamicElem.textContent, dynamicElem2.textContent];
 
 function randomString() {
-  const dynamicText = stringArr[0];
-  const dynamicTextArr = dynamicText.split("");
-  dynamicElem.textContent = "";
-  return dynamicTextArr;
+	const dynamicText = stringArr[0];
+	const dynamicTextArr = dynamicText.split("");
+	dynamicElem.textContent = "";
+	return dynamicTextArr;
 }
 function randomString2() {
-  const dynamicText2 = stringArr[1];
-  const dynamicTextArr2 = dynamicText2.split("");
-  dynamicElem2.textContent = "";
-  return dynamicTextArr2;
+	const dynamicText2 = stringArr[1];
+	const dynamicTextArr2 = dynamicText2.split("");
+	dynamicElem2.textContent = "";
+	return dynamicTextArr2;
 }
 
 function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function dynamic(Arr) {
-  dynamicElem2.textContent = "";
-  if (Arr.length > 0) {
-    dynamicElem.textContent += Arr.shift();
-    setTimeout(() => {
-      dynamic(Arr);
-    }, await delay(350));
-  } else {
-    dynamic2(randomString2());
-    setTimeout(() => {
-      reset();
-    }, 5000);
-  }
+	dynamicElem2.textContent = "";
+	if (Arr.length > 0) {
+		dynamicElem.textContent += Arr.shift();
+		setTimeout(() => {
+			dynamic(Arr);
+		}, await delay(350));
+	} else {
+		dynamic2(randomString2());
+		setTimeout(() => {
+			reset();
+		}, 5000);
+	}
 }
 dynamic(randomString());
 
 async function dynamic2(Arr2) {
-  if (Arr2.length > 0) {
-    dynamicElem2.textContent += Arr2.shift();
-    setTimeout(() => {
-      dynamic2(Arr2);
-    }, await delay(250));
-  } else {
-    // setTimeout(() => {
-    //     reset()
-    // }, 5800);
-  }
+	if (Arr2.length > 0) {
+		dynamicElem2.textContent += Arr2.shift();
+		setTimeout(() => {
+			dynamic2(Arr2);
+		}, await delay(250));
+	} else {
+		// setTimeout(() => {
+		//     reset()
+		// }, 5800);
+	}
 }
 
 function reset() {
-  dynamicElem.textContent = "";
-  dynamicElem2.textContent = "";
-  dynamic(randomString());
-  // dynamic2(randomString2());
+	dynamicElem.textContent = "";
+	dynamicElem2.textContent = "";
+	dynamic(randomString());
+	// dynamic2(randomString2());
 }
